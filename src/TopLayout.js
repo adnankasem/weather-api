7544ef07ffcb54748ee7c405ebd2c83bc6cfe255
Adnan Kasem <adnankasem2017@gmail.com>
2021-10-01 15:20:13 -0700

    Form input styling and TopLayout Component

--- src/TopLayout.js
+++ src/TopLayout.js
@@ -20,7 +20,6 @@ export default function TopLayout() {
 
     const handleKeyPress = async (e) => {
         if (e.key === 'Enter') {
-            e.preventDefault()
             console.log('Enter Pressed')
             let data = await fetchData(query)
 
@@ -33,21 +32,32 @@ export default function TopLayout() {
 
     return (
         <>
-            <div>
-                <h1>Weather App</h1>
-                <input
-                    type='text'
-                    value={query}
-                    onChange={(e) => setQuery(e.target.value)}
-                    onKeyPress={handleKeyPress}
-                    placeholder='Search Your City'
-                />
-            </div>
-            <div>
-                {data.name}{data.sys.country}
-                {data.main.temp}
-                {data.weather[0].main}
-                {data.weather[0].icon}
+
+            <div className='container'>
+                <div className='input'>
+                    <input
+                        className='input__box'
+                        type='text'
+                        value={query}
+                        onChange={(e) => setQuery(e.target.value)}
+                        onKeyPress={handleKeyPress}
+                        placeholder='Search a City...'
+                    />
+                </div>
+                {data.weather && (
+                    <>
+                        <h2>
+                            <span>{data.name}</span>
+                            <sup>{data.sys.country}</sup>
+                        </h2>
+
+                        <div className='weather'>
+
+                            <div className='info'>{data.weather[0].main}</div><div><img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} /></div>
+                            <div className='temp'><h2>{Math.round(data.main.temp)} </h2> <sup>&deg;C</sup></div>
+                        </div>
+                    </>
+                )}
             </div>
         </>
     )
